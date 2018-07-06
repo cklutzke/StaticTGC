5b88b85bdc599723a9144cd3630f518046543f9e
Carl Klutzke <carl@phos.net>
2018-07-06 17:12:38 -0400

    Prettier login controls.

--- src/components/TGCSession.vue
+++ src/components/TGCSession.vue
@@ -2,18 +2,27 @@
 The TGCSession component is responsible for creating session connections to The Game Crafter.
 It controls the tgc_session_id value in localStorage.
 Props: n/a
-TODO: Fix warning re: Unknown custom element: <b-progress>
-TODO: Display current session info more appropriately.
-  When logged in, show a "user" glyphicon, followed by user's name, on a dropdown that allows logout.
-  When not logged in, show a Log In link that can be clicked to request credentials.
-TODO: Events: sessionBegin, sessionEnd
+TODO: Events: session-begin, session-end
+  Need to emit from session's event handlers, but in those methonds "this" has
+  the context of session, with no way to reference the component and its $event object.
+TODO: Allow login via Facebook
+TODO: Allow use to create a new account
  -->
 
 <template>
-  <div id="TGCSession">
-    <div id="login">
-      <b-form @submit="onLoginSubmit" @reset="onLoginReset" v-show="session.properties.id == null">
-        <h1>Log In</h1>
+  <div>
+    <div id="LoggedIn" v-if="loggedIn">
+      <b-button-group>
+        <!-- HACK: Embed the icon below within the actual dropdown. -->
+        <b-button variant="secondary"><i class="fa fa-user fa-fw"></i></b-button>
+        <b-dropdown :text="userName">
+          <b-dropdown-item @click="logOutClick">Log Out</b-dropdown-item>
+        </b-dropdown>
+      </b-button-group>
+    </div>
+    <div id="NotLoggedIn" v-else>
+      <b-button variant="primary" @click="onShowLogin" v-if="!showLoginControls">Log In</b-button>
+      <b-form @submit="onLoginSubmit" @reset="onLoginReset" v-else>
         <b-form-group id="userNameInputGroup" label="User Name:" label-for="userNameInput">
           <b-form-input id="userNameInput" type="text" v-model="username" required
             autocomplete="username" placeholder="Enter username">
@@ -24,20 +33,11 @@ TODO: Events: sessionBegin, sessionEnd
             autocomplete="current-password" placeholder="Enter password">
           </b-form-input>
         </b-form-group>
-        <b-button type="submit" variant="primary">Submit</b-button>
-        <b-button type="reset" variant="secondary">Reset</b-button>
-        <p />
+        <b-button type="submit" variant="primary">Log In</b-button>
+        <b-button type="reset" variant="secondary">Cancel</b-button>
       </b-form>
     </div>
-    <div id="sessionData">
-      <h1>Session Info</h1>
-      <p>Session ID: {{ session.properties.id }}</p>
-      <p>User ID: {{ session.properties.user_id }}</p>
-      <p>User Name: {{ userName }}</p>
-      <b-button variant="secondary" @click="logOutClick" v-if="session.properties.id != null">
-        Log Out
-      </b-button>
-    </div>
+
   </div>
 </template>
 
@@ -49,58 +49,67 @@ const StaticTGC_api_key_id = "034F04B4-7329-11E8-BA7A-8BFD93A6FE1D";
 
 export default {
   name: 'TGCSession',
-  props: {
-  },
   data: function () {
     return {
+      showLoginControls: false,
       username: "carl@phos.net", // TEMP: This is here for convenience, remove it later.
       password: "statictgc", // TEMP: This is here for convenience, remove it later.
       session: wing.object({
-          with_credentials: false,
-          create_api: "/api/session",
-          on_create: function(properties) {
-            localStorage.setItem("tgc_session_id", properties.id);
-          },
-          on_delete: function(properties) {
-            localStorage.removeItem("tgc_session_id");
-          },
-          fetch_api: "/api/session/" + localStorage.getItem("tgc_session_id"),
-          params: {
-              _include_related_objects: ["user"],
-              api_key_id: StaticTGC_api_key_id
-          }
+        with_credentials: false,
+        create_api: "/api/session",
+        on_create: function(properties) {
+          localStorage.setItem("tgc_session_id", properties.id);
+        },
+        fetch_api: "/api/session/" + localStorage.getItem("tgc_session_id"),
+        on_fetch: function(properties) {
+        },
+        on_delete: function(properties) {
+          localStorage.removeItem("tgc_session_id");
+        },
+        params: {
+          _include_related_objects: ["user"],
+          api_key_id: StaticTGC_api_key_id
+        }
       })
     }
   },
   computed: {
+    loggedIn: function() {
+      return (this.session.properties.id != null);
+    },
     userName: function() {
       if (this.session.properties.user === undefined) {
-        return "Unknown";
+        return "Log In";
       } else {
         return this.session.properties.user.display_name;
       }
-    },
+    }
   },
   methods: {
+    onShowLogin (evt) {
+      this.showLoginControls = true;
+    },
     onLoginSubmit (evt) {
-        evt.preventDefault();
-        this.session.create({
-            username: this.username,
-            password: this.password
-        });
+      evt.preventDefault();
+      this.session.create({
+        username: this.username,
+        password: this.password
+      });
     },
     onLoginReset (evt) {
-        evt.preventDefault();
-        this.username = "";
-        this.password = "";
+      evt.preventDefault();
+      this.username = "";
+      this.password = "";
+      this.showLoginControls = false;
     },
-    logOutClick: function(event) {
-        this.session.delete();
+    logOutClick (evt) {
+      this.session.delete();
+      this.showLoginControls = false;
     }
   },
   mounted() {
     if (localStorage.getItem("tgc_session_id")) {
-        this.session.fetch();
+      this.session.fetch();
     }
   }
 }
