ac68c970781fb1da13a2a3ac2bbb5d4a86ce4d01
Carl Klutzke <carl@phos.net>
2018-07-10 14:35:36 -0400

    Loads new part data every time the partId changes.

--- src/components/PartViewer.vue
+++ src/components/PartViewer.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    // TODO: Show useful info about the part.
+    // TODO: Ensure the part is displayed if the user pastes in the URL or refreshes the page.
     <p>Selected part ID is {{ partId }}</p>
     <p>Returned part ID is {{ part.properties.id }}</p>
     <p>Returned part name is {{ part.properties.name }}</p>
@@ -18,15 +20,23 @@ export default {
   ],
   data: function () {
     return {
-      part: wing.object ({
-        with_credentials: false
-      })
+      // We can't reuse the same wing part object over and over. We have to
+      // start with an empty part and replace it when the ID changes.
+      part: wing.object({})
     }
   },
   watch: {
     partId: function (val) {
-      this.part.fetch_api = "/api/part/" + this.partId;
-      console.log(this.part.fetch_api);
+      this.onLoadPart();
+    }
+  },
+  methods: {
+    onLoadPart: function () {
+      this.part = wing.object ({
+        with_credentials: false,
+        fetch_api: "/api/part/" + this.partId
+      });
+
       this.part.fetch();
     }
   }
