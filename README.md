2eddc11489a4ae11438754cf821d6818ae9d3874
Carl Klutzke <carl@phos.net>
2018-07-24 10:03:05 -0400

    Search results display with ribbons. No links yet.

--- README.md
+++ README.md
@@ -16,55 +16,21 @@ GETTING STARTED ---------------------
 TODO List --------------------------
 
 Plan an appropriate landing page.
-  Show a product viewer like https://www.thegamecrafter.com/parts
-    Copy what JT is doing in the VUE branch of /plainblack/TGC
-      Translate following HTML (react) template to VUE:
-        https://github.com/plainblack/TGC/blob/vue/views/instantsearch_hit_template_js_include.tt
-
-        <div class="product is-gray">
-            <a href="{{shop_uri}}[% IF dept_uri %]?dept_uri=[% dept_uri %]&dept_name=[% dept_name %][% END %]" class="image d-flex align-items-center justify-content-center">
-            {{#on_sale}}<div class="ribbon ribbon-danger text-uppercase">{{^badge}}On Sale!{{/badge}}{{#badge}}{{badge}}{{/badge}}</div>{{/on_sale}}
-            {{#featured}}<div class="ribbon ribbon-warning text-uppercase">Featured</div>{{/featured}}
-            {{#staff_pick}}<div class="ribbon ribbon-success text-uppercase">Staff Pick</div>{{/staff_pick}}
-            {{#vintage}}<div class="ribbon ribbon-info text-uppercase">Vintage</div>{{/vintage}}
-                <img src="{{preview_uri}}" alt="product" class="img-fluid">
-            </a>
-            <div class="title">
-                <small class="text-muted"><a href="[% IF dept_uri %][% dept_uri %][% ELSE %]{{department_uri_part}}[% END %]">[% IF dept_name %][% dept_name %][% ELSE %]{{department_name}}[% END %]</a></small>
-                <a href="{{shop_uri}}[% IF dept_uri %]?dept_uri=[% dept_uri %]&dept_name=[% dept_name %][% END %]"><h3 class="h6 text-uppercase no-margin-bottom">{{name}}</h3></a>
-                {{^on_sale}}<button onclick="tgc.add_to_cart('{{sku_id}}', this, {{over18}})" class="btn btn-secondary btn-sm float-right"><i class="fas fa-cart-plus"></i> Add to Cart</button>{{/on_sale}}
-                {{#on_sale}}<a href="{{shop_uri}}[% IF dept_uri %]?dept_uri=[% dept_uri %]&dept_name=[% dept_name %][% END %]" class="btn btn-secondary btn-sm float-right"><i class="fas fa-cart-plus"></i> Add to Cart</a>{{/on_sale}}
-                <span class="price text-muted">{{price}}</span>
-            </div>
-        </div>
-
-      Copy expample code from search.addWidget() in views/search.tt, to assign CSS classes so they use tgc.css: Something like...
-        <div class="row">
-           <div class="col-6 col-xl-3 col-lg-4" v-for="result in results">
-               template stuff goes here
-           </div>
-        </div>
-
-    QUESTIONS
-      It seems like Algolia is fucking up the Bootstrap Vue b-cards.
-        Cards are supposed to be the same height, but they're not.
-        When I try to make the cards into components inside Algolia results, it says they're not registered correctly.
-        I can't invoke a computed value to massage / truncate product description text.
-          Or handle the hyperlink in the sticker pawn description text.
-        I can't invoke a method for the price button.
-      How do I make it so clicking anywhere on the card takes you to the product?
-        If I wrap it in a b-link, it turns all the text blue.
-      Why does the Department refinement list show kebab tags instead of human-readable text?
-    IDEAS
-      Can I show something about sales levels to provide social proof?
+  Copy what JT is doing in the VUE branch of /plainblack/TGC
+    Get links working: first to TGC pages, then to router views.
   Design a resizeable header
+
+  QUESTIONS
+    How can I find an example of a featured product, so I can test that ribbon?
+    Why does the Department refinement list show kebab tags instead of human-readable text?
+    Can we show something about sales levels to provide social proof?
 
 Create config file that specifies a logo, site name, search facets, CSS styling
   Config what items to show in search results: e.g. Category vs Department
   Derive nav menu from departments and categories
-    Look at Algolia search results to see what filters we can specify.
 
 Complete shopping process.
+  Allow user to add to cart from price button on product card.
   Allow a customer to complete a purchase.
     Assume we will redirect to The Game Crafter to complete the purchase.
   Display full cart contents in a popover from the cart icon.
@@ -83,3 +49,45 @@ Add gift certificate features.
 Eliminate flashes of unstyled / unarranged content.
 
 Update the page title and meta tags based from router changes. https://alligator.io/vuejs/vue-router-modify-head/
+
+PRODUCT SEARCH RESULT ATTRIBUTES ----------------------------
+_highlightResult
+badge
+category
+color
+contains_gore
+contains_nudity
+contains_profanity
+contains_sexuality
+contains_substance_abuse
+date_published
+department[array]
+department_name
+department_uri_part
+description
+discontinued
+family
+id
+keywords
+last_sale_date
+low_volume
+material
+name
+number_of_sides
+objectID
+object_type
+on_sale
+photo_uri
+preview_uri
+price
+quantity
+quantity_available_for_games
+quantity_available_for_shop
+rank
+sales
+shop_uri
+size_in_mm
+sku_id
+staff_pick
+usable_in_games
+vintage
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,6 @@
   <div class="home">
     <!-- TODO: Put a promotional banner / carousel here. -->
 
-    <!-- TODO: WTF can't I use tgc-product-card in the results display but I can use it here? -->
-    <!-- <tgc-product-card :product="testProduct" /> -->
-
     <ais-index
       app-id="5V0BUFDX8J"
       api-key="a25692c12853aea7a77c5a7125498512"
@@ -28,57 +25,46 @@
                 <ais-highlight :result="result" attribute-name="name"></ais-highlight>
               </router-link>
             </p> -->
-            <!-- <b-link :href="'https://thegamecrafter.com' + result.shop_uri" target="_blank"> -->
 
             <ais-results inline-template>
 
               <div class="row">
                 <div class="col-6 col-xl-3 col-lg-4" v-for="result in results">
                   <div class="product is-gray">
-                    <!-- <a href="{{shop_uri}}[% IF dept_uri %]?dept_uri=[% dept_uri %]&dept_name=[% dept_name %][% END %]" class="image d-flex align-items-center justify-content-center">
-                     {{#on_sale}}<div class="ribbon ribbon-danger text-uppercase">{{^badge}}On Sale!{{/badge}}{{#badge}}{{badge}}{{/badge}}</div>{{/on_sale}}
-                     {{#featured}}<div class="ribbon ribbon-warning text-uppercase">Featured</div>{{/featured}}
-                     {{#staff_pick}}<div class="ribbon ribbon-success text-uppercase">Staff Pick</div>{{/staff_pick}}
-                     {{#vintage}}<div class="ribbon ribbon-info text-uppercase">Vintage</div>{{/vintage}}
-                         <img src="{{preview_uri}}" alt="product" class="img-fluid">
-                     </a> -->
+                    <a href="#"
+                      class="image d-flex align-items-center justify-content-center">
+                      <!-- {{shop_uri}}[% IF dept_uri %]?dept_uri=[% dept_uri %]&dept_name=[% dept_name %][% END %] -->
+                        <div v-if="result.on_sale" class="ribbon ribbon-danger text-uppercase">
+                          {{result.badge ? result.badge : 'On Sale!'}}
+                        </div>
+                        <div v-if="result.featured" class="ribbon ribbon-warning text-uppercase">Featured</div>
+                        <div v-if="result.staff_pick" class="ribbon ribbon-success text-uppercase">Staff Pick</div>
+                        <div v-if="result.vintage" class="ribbon ribbon-info text-uppercase">Vintage</div>
+                        <img :src="result.preview_uri" alt="product" class="img-fluid">
+                    </a>
                     <div class="title">
-                      <!-- <small class="text-muted"><a href="[% IF dept_uri %][% dept_uri %][% ELSE %]{{department_uri_part}}[% END %]">[% IF dept_name %][% dept_name %][% ELSE %]{{department_name}}[% END %]</a></small> -->
+                      <small class="text-muted">
+                        <!-- <a href="[% IF dept_uri %][% dept_uri %][% ELSE %]{{department_uri_part}}[% END %]"> -->
+                        {{ result.dept_name ? result.dept_name : result.department_name }}
+                        <!-- </a> -->
+                      </small>
                       <!-- <a href="{{shop_uri}}[% IF dept_uri %]?dept_uri=[% dept_uri %]&dept_name=[% dept_name %][% END %]"><h3 class="h6 text-uppercase no-margin-bottom">{{name}}</h3></a> -->
                       <h3 class="h6 text-uppercase no-margin-bottom">{{result.name}}</h3>
-                      <!-- {{^on_sale}}<button onclick="tgc.add_to_cart('{{sku_id}}', this, {{over18}})" class="btn btn-secondary btn-sm float-right"><i class="fas fa-cart-plus"></i> Add to Cart</button>{{/on_sale}}
-                      {{#on_sale}}<a href="{{shop_uri}}[% IF dept_uri %]?dept_uri=[% dept_uri %]&dept_name=[% dept_name %][% END %]" class="btn btn-secondary btn-sm float-right"><i class="fas fa-cart-plus"></i> Add to Cart</a>{{/on_sale}} -->
+                      <button v-if="!result.on_sale"
+                        class="btn btn-secondary btn-sm float-right">
+                        <!-- onclick="tgc.add_to_cart('{{sku_id}}', this, {{over18}})" -->
+                        <i class="fas fa-cart-plus"></i> Add to Cart
+                      </button>
+                      <a v-else href="#"
+                        class="btn btn-secondary btn-sm float-right">
+                        <!-- {{shop_uri}}[% IF dept_uri %]?dept_uri=[% dept_uri %]&dept_name=[% dept_name %][% END %] -->
+                        <i class="fas fa-cart-plus"></i> Add to Cart
+                      </a>
                       <span class="price text-muted">{{result.price}}</span>
                     </div>
                   </div>
                 </div>
               </div>
-
-
-              <!-- <b-card-group deck>
-                <template v-for="result in results" >
-                  <b-card
-                    :title="result.name"
-                    :sub-title="result.category"
-                    :img-src="'https:' + result.preview_uri"
-                    :img-alt="'Image of ' + result.name"
-                    img-top
-                    :border-variant="result.badge ? 'primary' : 'secondary'"
-                    style="width: 10rem; padding-top: 30px"
-                    title-tag="h5" sub-title-tag="h6">
-                    <p class="card-text">
-                      {{ String(result.short_description ? result.short_description : result.description).substr(0,100) }}
-                    </p>
-                    <div slot="footer">
-                      <span>{{ result.badge }}</span>
-                      <b-button disabled variant="primary" @click="buyClick(test)">
-                        <span class="fas fa-cart-plus"></span>
-                        {{ "$" + Number(result.price).toFixed(2) }}
-                      </b-button>
-                    </div>
-                  </b-card>
-                </template>
-              </b-card-group> -->
 
             </ais-results>
           </b-col>
@@ -98,44 +84,14 @@ export default {
   },
   data: function () {
     return {
-      // testProduct: {
-      //   name: "test product",
-      //   preview_uri: "//s3.amazonaws.com/preview.thegamecrafter.com/45C414B4-11D2-11E8-AADE-153194A6FE1D.png",
-      //   price: 0.02
-      // },
       facets: [
         { name: "department", label: "Departments"},
         { name: "category", label: "Categories"},
         { name: "color", label: "Colors"}
-      ],
-      homeResults: [
-        { name: "The Captain is Dead",
-          category: "Indie Games",
-          preview_uri: "//s3.amazonaws.com/files.thegamecrafter.com/9eb5bf196bebafa33dbbed5e2aa33563772bfd10",
-          badge: "Staff Pick",
-          short_description: "The last 10 minutes of your favorite Sci-Fi show that has gone so badly that The Captain is Dead!",
-          description: "This is really long and nobody wants to read it.",
-          price: 49.99
-        },
-        { name: "Djin",
-          category: "Minifigs",
-          preview_uri: "//s3.amazonaws.com/preview.thegamecrafter.com/45C414B4-11D2-11E8-AADE-153194A6FE1D.png",
-          badge: "",
-          short_description: "It's cool.",
-          description: "This is really long and nobody wants to read it.",
-          price: 8.75
-        }
       ]
     }
   },
   methods: {
-    // TEMP: HTML event IDs should be in kebab case.
-    buyClick: function(sku_id) {
-      this.$emit("tgc-product-buy", {
-        sku_id: sku_id,
-        quantity: 1
-      })
-    }
   }
 
 }
@@ -144,22 +100,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
-/*
-  The card text is styled according to the following tags
-    Title is h4 (set via title-tag)
-    Subtitle is h6 (set via sub-title-tag)
-    Body and footer are div (set via body-tag and footer-tag)
-*/
-
-.cwk-css-class-test {
-  text-align: right;
-  color: blue;
-}
-
 .ais-refinement-list {
-}
-
-.card-text {
 }
 
 </style>
